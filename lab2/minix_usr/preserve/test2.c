f835f1d42d3d59fc5abf8ab40522f11983c1b731
pdobro <ptrkdobrowolski@gmail.com>
2020-04-06 09:44:28 +0200

    lah 2 commit

---
+++ lab2/minix_usr/preserve/test2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <time.h>
+#include <math.h>
+/*#include "test.h"*/
+int getpri(int pid){
+	message m;
+	m.m1_i1 = pid;
+	return _syscall(MM, GETPRI, &m);
+}
+
+void setpri(int pid, int group){
+	message m;
+	m.m1_i1 = pid;
+	m.m1_i2 = group;
+	_syscall(MM, SETPRI, &m);
+}
+
+int main(int argc, char* argv[]){
+
+	clock_t b, e;
+	double result;
+	int i, j, group;
+	setpri(getpid(), 2);
+
+
+	b = clock();
+	for(i = j = 0; i < 4000; i++){
+		/*j+=160;*/
+		sqrt(i);
+	}
+	e = clock();
+	result = ((double)(e - b))/CLOCKS_PER_SEC;
+
+	group = getpri(getpid());
+	printf("pid: %d\nprio: %d\ntime: %fs\n", getpid(),group, result);
+
+	return 0;
+}
